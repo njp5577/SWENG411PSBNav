92ab45999db47c891c256d162002c918d8581a5e
matt-hardenburg <mzh5947@psu.edu>
2023-11-25 00:21:02 -0500

    added password hashing functionality and reset all old passwords

--- app/src/main/java/com/example/loginpageassignment/appscreens/ResetPassword.kt
+++ app/src/main/java/com/example/loginpageassignment/appscreens/ResetPassword.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import com.example.loginpageassignment.R
 import com.example.loginpageassignment.parentpageclasses.LoggedOutPage
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import org.mindrot.jbcrypt.BCrypt
 
 class ResetPassword : LoggedOutPage() {
 
@@ -39,15 +41,23 @@ class ResetPassword : LoggedOutPage() {
         this.buttonReset = buttonReset
     }
 
+    private fun resetPassword(documents: QuerySnapshot, ipassword: String, iconfirm: String) {
+        if (ipassword == iconfirm && ipassword.length > 4) {
+            val hashedPassword = BCrypt.hashpw(ipassword, BCrypt.gensalt())
+            documents.documents[0].reference.update("password", hashedPassword).addOnSuccessListener {
+                Toast.makeText(this, "Password has been reset.", Toast.LENGTH_SHORT).show()
+                // Go to login screen
+                startActivity(Intent(this, SignIn::class.java))
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset)
 
-        var firestore: FirebaseFirestore;
-
-        firestore = FirebaseFirestore.getInstance()
-
-        var userRef = firestore.collection("Users")
+        val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+        val userRef = firestore.collection("Users")
 
         setEditTextUsernameFun(findViewById(R.id.editTextUsername))
         setEditTextPasswordFun(findViewById(R.id.editTextPassword))
@@ -55,43 +65,31 @@ class ResetPassword : LoggedOutPage() {
         setEditTextEmailFun(findViewById(R.id.editTextEmail))
         setButtonResetFun(findViewById(R.id.buttonReset))
         setButtonLoginFun(findViewById(R.id.buttonLogin))
+
         //If user wants to go back to sign in page
         getButtonLoginFun().setOnClickListener {
             val go = Intent(this, SignIn::class.java)
             startActivity(go)
         }
+
         //If user wants to reset password
         getButtonResetFun().setOnClickListener {
             // Validate the username and password
             val iusername = getEditTextUsernameFun().text.toString()
-            val ipassword = getEditTextPasswordFun().text.toString()
+            var ipassword = getEditTextPasswordFun().text.toString()
             val iemail = getEditTextEmailFun().text.toString()
             val iconfirm = getEditTextConfirmPasswordFun().text.toString()
-            var match = 0
 
             //Check all users to look for a match
             userRef.whereEqualTo("username", iusername).whereEqualTo("email", iemail).get().addOnSuccessListener{ documents ->
                 //Check if incorrect credentials
-                if ((documents.isEmpty)) {
+                if (documents.isEmpty)
+                {
                     Toast.makeText(this, "Invalid username or email.", Toast.LENGTH_SHORT).show()
-                    match = 0
                 }
-                else{
-                    match = 1
-                }
-
-                //Reset password if there are no issues
-                if(match == 1) {
-                    if (ipassword == iconfirm && ipassword.length > 3){
-                        documents.documents[0].reference.update("password", ipassword).addOnSuccessListener {
-                            Toast.makeText(this, "Password has been reset.", Toast.LENGTH_SHORT).show()
-                            // Go to login screen
-                            val go = Intent(this, SignIn::class.java)
-
-                            startActivity(go)
-                        }
-                    }
-
+                else
+                {
+                    resetPassword(documents, ipassword, iconfirm)
                 }
             }
         }
--- app/src/main/java/com/example/loginpageassignment/appscreens/SignIn.kt
+++ app/src/main/java/com/example/loginpageassignment/appscreens/SignIn.kt
@@ -8,8 +8,10 @@ import com.example.loginpageassignment.R
 import com.example.loginpageassignment.dataobjects.CurrentUser
 import com.example.loginpageassignment.parentpageclasses.LoggedOutPage
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.mindrot.jbcrypt.BCrypt
 
 class SignIn : LoggedOutPage() {
 
@@ -32,10 +34,29 @@ class SignIn : LoggedOutPage() {
         this.buttonSignUp = buttonSignUp
     }
 
+    private fun showInvalidCredentialsMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun navigateToHomePage(userType: String, username: String) {
+        val go: Intent = when (userType) {
+            "Admin" -> Intent(this, AdminHome::class.java)
+            "EventOrg" -> Intent(this, Homepage::class.java)
+            else -> Intent(this, Homepage::class.java)
+        }
+
+        val json = Json.encodeToString(CurrentUser(username))
+        go.putExtra("User", json)
+        startActivity(go)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         //Show the login page
         setContentView(R.layout.activity_login)
+
         //Get info from text inputs and buttons
         setEditTextUsernameFun(findViewById(R.id.editTextUsername))
         setEditTextPasswordFun(findViewById(R.id.editTextPassword))
@@ -43,72 +64,34 @@ class SignIn : LoggedOutPage() {
         setButtonSignUpFun(findViewById(R.id.buttonSignUp))
         setButtonResetFun(findViewById(R.id.buttonForgot))
 
-        var firestore: FirebaseFirestore;
-
-        firestore = FirebaseFirestore.getInstance()
-
-        var userRef = firestore.collection("Users")
+        val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+        val userRef = firestore.collection("Users")
 
         //When user wants to reset password
         getButtonResetFun().setOnClickListener {
-            val go = Intent(this, ResetPassword::class.java)
-            startActivity(go)
+            startActivity(Intent(this, ResetPassword::class.java))
         }
 
         //When user wants to sign up
         getButtonSignUpFun().setOnClickListener {
-            val go = Intent(this, SignUp::class.java)
-            startActivity(go)
+            startActivity(Intent(this, SignUp::class.java))
         }
 
         //When user wants to login
-        getButtonLoginFun().setOnClickListener {
+        getButtonLoginFun().setOnClickListener{
             // Validate the username and password
             val username = getEditTextUsernameFun().text.toString()
             val password = getEditTextPasswordFun().text.toString()
-            var notFound = 1
 
-            userRef.whereEqualTo("username", username).whereEqualTo("password", password).get().addOnSuccessListener{ documents ->
-                //Check that account exists
-                if ((documents.isEmpty)) {
-                    Toast.makeText(this, "Invalid username or password.", Toast.LENGTH_SHORT).show()
-                    notFound = 0
+            userRef.whereEqualTo("username", username).get().addOnSuccessListener{ documents ->
+                val hashed = documents.documents[0].getString("password")
+                if((documents.isEmpty || !BCrypt.checkpw(password, hashed))) //Check that account exists
+                {
+                    showInvalidCredentialsMessage("Invalid username or password.")
                 }
-
-
-                if(notFound == 1) {
-
-                    if (documents.documents[0].get("type") == "Admin"){
-                        // Go to admin homepage
-                        val go = Intent(this, AdminHome::class.java)
-
-                        val json = Json.encodeToString(CurrentUser(username))
-
-                        go.putExtra("User", json)
-
-                        startActivity(go)
-                    }
-                    else if (documents.documents[0].get("type") == "EventOrg"){
-                        // Go to event organizer homepage
-                        val go = Intent(this, Homepage::class.java)
-
-                        val json = Json.encodeToString(CurrentUser(username))
-
-                        go.putExtra("User", json)
-
-                        startActivity(go)
-                    }
-                    else{
-                        // Go to user homepage
-                        val go = Intent(this, Homepage::class.java)
-
-                        val json = Json.encodeToString(CurrentUser(username))
-
-                        go.putExtra("User", json)
-
-                        startActivity(go)
-                    }
-
+                else //if account exists, verify password
+                {
+                    navigateToHomePage(documents.documents[0].getString("type")!!, username)
                 }
             }
         }
