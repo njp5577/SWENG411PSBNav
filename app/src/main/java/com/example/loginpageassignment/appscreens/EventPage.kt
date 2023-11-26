366f677132e72016c97202b0ddd51643014ccf72
matt-hardenburg <mzh5947@psu.edu>
2023-11-25 21:41:19 -0500

    added comments, fixed formatting

--- app/src/main/java/com/example/loginpageassignment/appscreens/EventPage.kt
+++ app/src/main/java/com/example/loginpageassignment/appscreens/EventPage.kt
@@ -17,22 +17,18 @@ import com.example.loginpageassignment.utilities.popup.EventsPopup
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.serialization.json.Json
 
-class EventPage : LoggedInPage() {
-
+class EventPage : LoggedInPage()
+{
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
-
-
     private val eventRef = FirebaseFirestore.getInstance().collection("Events")
     private val events = mutableListOf<PSB_Event>()
     private val eventsPopup = EventsPopup(this)
-
     private var limit = 5 // Initial limit
     private var loading = false
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
-        Log.d("EventPage", "Entry")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
 
@@ -47,6 +43,7 @@ class EventPage : LoggedInPage() {
         // Adapter with CardView items
         adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>()
         {
+            // Create ViewHolder
             override fun onCreateViewHolder(parent: ViewGroup,
                                             viewType: Int): RecyclerView.ViewHolder
             {
@@ -57,11 +54,13 @@ class EventPage : LoggedInPage() {
                 return object : RecyclerView.ViewHolder(view) {}
             }
 
+            // Return the number of items in the list
             override fun getItemCount(): Int
             {
                 return events.size
             }
 
+            // Bind data to ViewHolder
             override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
                 val eventCard = holder.itemView.findViewById<CardView>(R.id.eventCard)
                 val eventData = getItem(position)
@@ -71,32 +70,29 @@ class EventPage : LoggedInPage() {
 
         recyclerView.adapter = adapter
         loadData()
-
         Log.d("EventPage", "Events size: ${events.size}")
 
         // Set up scroll listener
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int)
+            {
                 super.onScrolled(recyclerView, dx, dy)
 
+                // Get the last visible item position
                 val layoutManager = recyclerView.layoutManager as LinearLayoutManager
                 val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
                 val totalItemCount = layoutManager.itemCount
 
-                if (!loading && lastVisibleItemPosition == totalItemCount - 1) {
-                    // Load more data when user scrolls to the bottom
-                    loadMoreData()
-                }
+                // Load more data when user scrolls to the bottom
+                if (!loading && lastVisibleItemPosition == totalItemCount - 1) loadMoreData()
             }
         })
     }
 
-    private fun getItem(position: Int): PSB_Event
-    {
-        // Return event data
-        return events[position]
-    }
+    // Get item at a specific position
+    private fun getItem(position: Int): PSB_Event { return events[position] }
 
+    // Bind event data to the CardView
     private fun bindEventData(eventCard: CardView, event: PSB_Event)
     {
         // Bind event data to the card view
@@ -105,16 +101,19 @@ class EventPage : LoggedInPage() {
         eventCard.findViewById<TextView>(R.id.nameTextView).text = event.eventName
         eventCard.findViewById<TextView>(R.id.timeTextView).text = event.eventTime
 
+        // Show event details popup when the card is clicked
         eventCard.setOnClickListener{eventsPopup.showDetails(event, this.getLoggedInAsFun())}
     }
 
+    // Load initial data from database
     private fun loadData()
     {
         loading = true
         eventRef.limit(limit.toLong()).get().addOnSuccessListener{ documents ->
             Log.d("EventPage", "Document count: ${documents.size()}")
             val newPSBEvents = mutableListOf<PSB_Event>()
-            for (document in documents)
+
+            for (document in documents)  // Convert database documents to PSB_Event objects
             {
                 val event = document.toObject(PSB_Event::class.java)
                 newPSBEvents.add(event)
@@ -122,7 +121,6 @@ class EventPage : LoggedInPage() {
 
             // Sort the events by date and time
             newPSBEvents.sortWith(compareBy({ it.eventDate }, { it.eventTime }))
-
             loading = false
             updateAdapterData(newPSBEvents)
         }.addOnFailureListener{
@@ -131,13 +129,14 @@ class EventPage : LoggedInPage() {
         }
     }
 
+    // Load more data when the user scrolls to the bottom
     private fun loadMoreData()
     {
         limit += 5
         loadData()
     }
 
-    //update the adapter data
+    // Update the adapter data
     @SuppressLint("NotifyDataSetChanged")
     private fun updateAdapterData(newPSBEvents: List<PSB_Event>)
     {
